6738ea2daf8b04845868bb03d75b62157e54a11e
River K <nightwingsking25@gmail.com>
2025-04-21 23:41:01 -0700

    made an introduction prog

---
+++ intro.py
@@ -0,0 +1,9 @@
+def printInfo():
+    print("My name is River Kinley, and my pronouns are he/him. My favorite movie is Kung Fu Hustle, I think its very funny! " \
+    "My favorite food is Korean BBQ as I like to try to get my moneys worth and fail.")
+
+def main():
+    printInfo()
+
+if __name__=="__main__":
+    main()
