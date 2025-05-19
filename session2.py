be055c5df61b2b762eb7ed749ff83408fb29f6b9
River K <nightwingsking25@gmail.com>
2025-05-18 21:55:28 -0700

    code journal #2

--- session2.py
+++ session2.py
@@ -3,19 +3,19 @@ def main():
     float1 = 2.3
     float2 = 12.2
     sum = float1 + float2 
-    print(f"Sum of two floats: {sum}")
+    print(f"Sum of two floats: {sum} {type(sum)}")
 
     #initialize 2 ints and find the difference between them
     int1 = 1
     int2 = 247
     sum2 = int2 - int1
-    print(f"Difference of two ints: {sum2}")
+    print(f"Difference of two ints: {sum2} {type(sum2)}")
 
     #initalize a float and an int and add them
     float3 = 27.3
     int3 = 10
     sum3 = float3 * int3
-    print(f"Product of a float and an int {sum3}")
+    print(f"Product of a float and an int: {sum3} {type(sum3)}")
 
 if __name__ == "__main__":
     main()
--- session5.py
+++ session5.py
@@ -1,22 +1,26 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
+from tabulate import tabulate
+from astropy.table import Table
+
+
+# make array of values from 0 to 2pi with 1000 values
+x = np.linspace(0, 2 * np.pi , 1000)
+
+# we are looking for sin(x)
+y = np.sin(x)
+
+# create pairs containing our x and y values
+table_entries = [(a, b) for a, b in zip(x, y)]
+
+# make the table
+table_headers = ["x","y"]
+table = tabulate(table_entries, tablefmt="grid", headers=table_headers, floatfmt=".4f")
+
 
 def main():
-    
-    entries =  1000
-    
-    beg = 0
-    end = 2 * math.pi
-
-    inc = (end - beg) / (entries - 1)
-
-    print("  x     sin(x)") 
-    print("-" * 22)
-
-    for i in range(entries):
-        x = beg + i * inc
-        sinx = math.sin(x)
-        print(f"{x}     {sinx}")
-
+    print(table)
 
 if __name__ == "__main__":
     main()
